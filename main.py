1b1cc728a9937265c31b3e81d2585fe637a8a64f
alexjwick <alexjwick03@gmail.com>
2024-01-14 16:07:26 -0500

    switched to adafruit for readings

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-import RPi.GPIO as GPIO
-import dht11
-import time
-import datetime
+import Adafruit_DHT
 
-# initialize GPIO
-GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BCM)
+sensor = Adafruit_DHT.DHT11
 
-# read data using pin 17
-instance = dht11.DHT11(pin=17)
+pin = 4
 
-try:
-	while True:
-		result = instance.read()
-		if result.is_valid():
-			print("Last valid input: " + str(datetime.datetime.now()))
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-			print("Temperature: %-3.1f C" % result.temperature)
-			print("Temperature: %-3.1f C" % (result.temperature * 9/5) + 32)
-			print("Humidity: %-3.1f %%" % result.humidity)
-		
-		time.sleep(6)
-
-except KeyboardInterrupt:
-	print("Cleanup")
-	GPIO.cleanup()
+if humidity is not None and temperature is not None:
+	print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+	print('Failed to get reading. Try again!')
