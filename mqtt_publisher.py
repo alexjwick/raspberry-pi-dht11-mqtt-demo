a34937af6f3c73c3620f56f6cd70ab14973562cc
alexjwick <alexjwick03@gmail.com>
2024-01-16 14:11:47 -0500

    connected sensor to mqtt publisher

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,4 +1,9 @@
 import paho.mqtt.client as paho
+import Adafruit_DHT
+import time
+from Sensor import Sensor
+
+sensor = Sensor(Adafruit_DHT.DHT11, 4)
 
 client = paho.Client()
 
@@ -6,5 +11,16 @@ if client.connect("localhost", 1883, 60) != 0:
     print("Couldn't connect to the mqtt broker")
     exit(1)
 
-client.publish("test", "Hello World!")
+while True:
+    try:
+        humidity, temperature = sensor.get_reading()
+        datetime = time.strftime("%Y-%m-%d %H:%M:%S")
+        client.publish("sensor", "{0} Temp: {1:0.1f}*C Humidity: {2:0.1f}%".format(datetime, temperature, humidity))
+        time.sleep(5)
+    except KeyboardInterrupt:
+        print("Exiting")
+        break
+    except Exception as e:
+        print(e)
+        break
 client.disconnect()
